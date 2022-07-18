fc7899da9c248f8b6ca369f7f16df9f2117d9490
EleenTyasi <106459601+EleenTyasi@users.noreply.github.com>
2022-07-18 16:23:47 -0700

    dialogue removed for now game over music implemented

---
+++ assets/data/spicedshowdown/spicedshowdown pressure.lua
@@ -0,0 +1,19 @@
+local pressure = 0.01
+function noteMiss()
+    if pressure >= 0.01 then
+        pressure = pressure + 0.01;
+    end
+end
+
+function goodNoteHit()
+end
+
+function onBeatHit()
+        function opponentNoteHit()
+            health = getProperty('health')
+            if getProperty('health') > 0.50 then
+                setProperty('health', health-(pressure * 0.5));
+                
+            end
+        end
+end
---
+++ mods/vs. flora/scripts/death.lua
@@ -0,0 +1,5 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'death'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameover'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'maxkek'); --put in mods/music/
+end
