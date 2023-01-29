5f32cc0aae34426986b564fb5c7ef473e1ddcc0e
EleenTyasi <106459601+EleenTyasi@users.noreply.github.com>
2023-01-29 13:11:52 -0800

    noot

--- mods/vs. flora/custom_notetypes/Puppet Note Hard.lua
+++ mods/vs. flora/custom_notetypes/Puppet Note Hard.lua
@@ -1,10 +1,10 @@
 function onCreate()
 	--Iterate over all notes
 	for i = 0, getProperty('unspawnNotes.length')-1 do
-		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Heal Note' then  --Checks if the note is the one in the script. Set this to the name of your file.
-			setPropertyFromGroup('unspawnNotes', i, 'texture', 'HEALNOTE_assets'); --Changes the texture to your own
-			setPropertyFromGroup('unspawnNotes', i, 'hitHealth', '0.6555'); --Default is 0.023, sets the value you get on hit
-			setPropertyFromGroup('unspawnNotes', i, 'missHealth', '0.005'); --Default is 0.0475, sets the value you get on miss
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Puppet Note Hard' then  --Checks if the note is the one in the script. Set this to the name of your file.
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'PUPPETNOTE_assets'); --Changes the texture to your own
+			setPropertyFromGroup('unspawnNotes', i, 'hitHealth', '0.044'); --Default is 0.023, sets the value you get on hit
+			setPropertyFromGroup('unspawnNotes', i, 'missHealth', '1'); --Default is 0.0475, sets the value you get on miss
 			setPropertyFromGroup('unspawnNotes', i, 'hitCausesMiss', false);
 
 			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') then --Doesn't let Dad/Opponent notes get ignored
@@ -20,7 +20,7 @@ end
 -- noteType: The note type string/tag
 -- isSustainNote: If it's a hold note, can be either true or false
 function goodNoteHit(id, noteData, noteType, isSustainNote)
-	if noteType == 'Heal Note' then
+	if noteType == 'Crystal Note' then
 		-- put something here if you want
 	end
 end
@@ -28,7 +28,7 @@ end
 -- Called after the note miss calculations
 -- Player missed a note by letting it go offscreen
 function noteMiss(id, noteData, noteType, isSustainNote)
-	if noteType == 'Heal Note' then
+	if noteType == 'Crystal Note' then
 		-- put something here if you want
 	end
 end
