f55645abcc2d5fb2f874a02a7647dee4604f86aa
EleenTyasi <106459601+EleenTyasi@users.noreply.github.com>
2023-03-02 18:14:22 -0800

    LOTS of edits

--- mods/custom_notetypes/Puppet Note Normal.lua
+++ mods/custom_notetypes/Puppet Note Normal.lua
@@ -1,10 +1,10 @@
 function onCreate()
 	--Iterate over all notes
 	for i = 0, getProperty('unspawnNotes.length')-1 do
-		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Puppet Note Hard' then  --Checks if the note is the one in the script. Set this to the name of your file.
-			setPropertyFromGroup('unspawnNotes', i, 'texture', 'PUPPETNOTE_assets'); --Changes the texture to your own
-			setPropertyFromGroup('unspawnNotes', i, 'hitHealth', '0.044'); --Default is 0.023, sets the value you get on hit
-			setPropertyFromGroup('unspawnNotes', i, 'missHealth', '1'); --Default is 0.0475, sets the value you get on miss
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Puppet Note Normal' then  --Checks if the note is the one in the script. Set this to the name of your file.
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'puppetnote'); --Changes the texture to your own
+			setPropertyFromGroup('unspawnNotes', i, 'hitHealth', '0.023'); --Default is 0.023, sets the value you get on hit
+			setPropertyFromGroup('unspawnNotes', i, 'missHealth', '0.057'); --Default is 0.0475, sets the value you get on miss
 			setPropertyFromGroup('unspawnNotes', i, 'hitCausesMiss', false);
 
 			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') then --Doesn't let Dad/Opponent notes get ignored
