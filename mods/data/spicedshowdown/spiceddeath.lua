e43aacc11bf0e713d677c941bab49f39db233b94
EleenTyasi <106459601+EleenTyasi@users.noreply.github.com>
2023-03-10 23:53:46 -0800

    so get your face ready

--- mods/data/spicedshowdown/spiceddeath.lua
+++
@@ -1,5 +0,0 @@
-function onCreate()
-	-- Bonnie: That'll teach you a lesson in humility, you blue haired jerk!
-	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'spiceddeath'); --put in mods/sounds/
-
-end
