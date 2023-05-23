774e180288dc567516a9fa676c46a7afd8ab0a19
EleenTyasi <106459601+EleenTyasi@users.noreply.github.com>
2023-05-23 06:51:22 -0700

    removed duplicate charts added pressure to week 2.

---
+++ mods/data/belpop/pressure.lua
@@ -0,0 +1,20 @@
+local pressure = 0.01
+function noteMiss()
+    if pressure >= 0.01 then
+        pressure = pressure + 0.033;
+    end
+end
+
+function goodNoteHit()
+
+end
+
+function onBeatHit()
+        function opponentNoteHit()
+            health = getProperty('health')
+            if getProperty('health') > 0.7 then
+                setProperty('health', health-(pressure * 0.3 * healthLossMult));
+                
+            end
+        end
+end
